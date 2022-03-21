7571a149918d9ed2213a1fa8d8b8ad5a7d39b131
Jesus Leon <ing.jesusleon@gmail.com>
2022-03-21 00:17:43 -0400

    New: Implementacion del player

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
   #app
     jm-header
-    section.section
-      nav.nav.has-shadow
+    jm-notification(v-show="showNotification")
+      p(slot="body") No se encontrarón resultados
+    jm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
         .container
           input.input.is-large(
             type="text",
@@ -16,27 +19,42 @@
           small {{ searchMessage }}
 
       .container.results
-        .columns
-          .column(v-for="t in tracks") 
-            | {{ t.name }} - {{ t.artists[0].name }}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            jm-track(
+              :class="{ 'is-active': t.id === selectedTrack }",
+              :track="t", 
+              @select="setSelectedTrack")
     jm-footer
 </template>
 
 <script>
-import trackService from './services/track'
-import JmFooter from './components/layout/footer.vue'
-import JmHeader from './components/layout/header.vue'
+import trackService from '@/services/track'
+
+import JmFooter from '@/components/layout/Footer.vue'
+import JmHeader from '@/components/layout/Header.vue'
+
+import JmTrack from '@/components/track.vue'
+
+import JmNotification from '@/components/shared/Notification.vue'
+import JmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'App',
   components: {
     JmFooter,
-    JmHeader
+    JmHeader,
+    JmTrack,
+    JmLoader,
+    JmNotification
   },
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      isLoading: false,
+      selectedTrack: '',
+      showNotification: false
     }
   },
   computed: {
@@ -44,14 +62,28 @@ export default {
       return `Encontrados: ${this.tracks.length}`
     }
   },
-
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
   methods: {
     search () {
       if (!this.searchQuery) { return }
+      this.isLoading = true
       trackService.search(this.searchQuery)
         .then(res => {
+          this.showNotification = res.tracks.total === 0
           this.tracks = res.tracks.items
+          this.isLoading = false
         })
+    },
+    setSelectedTrack (trackId) {
+      this.selectedTrack = trackId
     }
   }
 }
@@ -61,5 +93,8 @@ export default {
   @import './scss/main.scss';
   .results {
     margin-top: 50px;
+  }
+  .is-active {
+    border: 3px #23d160 solid;
   }
 </style>
